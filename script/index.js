bb66b0f8494a91e86352a587a9769c52bafe8fa9
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-03-03 18:31:58 +0800

    Roll Dice

--- script/index.js
+++ script/index.js
@@ -12,6 +12,10 @@ document.getElementById("submit").addEventListener("click", () => {
   window.alert(`You are ${username}`);
 }); */
 
+/* 
+
+for inc des res
+
 let num = 0;
 
 function render() {
@@ -30,4 +34,19 @@ document.querySelector(".btn-success").addEventListener("click", () => {
 document.querySelector(".btn-primary").addEventListener("click", () => {
   num += 1;
   render();
+}); */
+
+let x;
+let y;
+let z;
+
+
+document.querySelector('.roll').addEventListener("click", () => {
+  x = Math.floor(Math.random() * 6) + 1;
+  y = Math.floor(Math.random() * 6) + 1;
+  z = Math.floor(Math.random() * 6) + 1;
+
+  document.querySelector('.x').textContent = x;
+  document.querySelector('.y').textContent = y;
+  document.querySelector('.z').textContent = z;
 });
