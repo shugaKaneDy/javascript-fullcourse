de6a0f65ceedc619d909ce62c52c5929a97cfc6a
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-03-11 21:25:54 +0800

    forEach method

--- script/index.js
+++ script/index.js
@@ -471,6 +471,58 @@ function displayPage(result) {
 }
 */
 
+/* 
+
+forEach
+
+element, index, array = tihs is automatic param
+
+let numbers = [1,2,3,4,5];
+
+numbers.forEach(cube);
+numbers.forEach(display);
+
+function double(element, index, array){
+  array[index] = element * 2;
+}
+
+function triple(element, index, array){
+  array[index] = element * 3;
+}
+
+function square(element, index, array){
+  array[index] = Math.pow(element, 2);
+}
+
+function cube(element, index, array){
+  array[index] = Math.pow(element, 3);
+}
+
+function display(element) {
+  console.log(element);
+}
+*/
+
+let fruits = ["apple", "orange", "banana", "coconut"];
+
+fruits.forEach(capitalized);
+fruits.forEach(display);
+
+function upperCase(element, index, array) {
+  array[index] = element.toUpperCase();
+}
+
+function lowerCase(element, index, array) {
+  array[index] = element.toLowerCase();
+}
+
+function capitalized(element, index, array) {
+  array[index] = element.charAt(0).toUpperCase() + element.slice(1);
+}
+
+function display(element) {
+  console.log(element);
+}
 
 
 
