265b804dab716ecb193d53d75e59b81fc944c4a0
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-03-13 01:38:48 +0800

    Function Expression

--- script/index.js
+++ script/index.js
@@ -680,3 +680,47 @@ function minGrade(accumulator, element) {
 }
 */
 
+/* 
+function declaration = define reusable block of code that performas a specific task
+
+function hello() {
+  console.log('hello');
+}
+
+function expression = a way to define functions as values or variables
+
+const hello = function() {
+  console.log('hello');
+}
+
+setTimeout(function() {
+  console.log('hello');
+}, 3000);
+
+*/
+
+const numbers = [1,2,3,4,5,6];
+const squares = numbers.map(function(element) {
+  return Math.pow(element, 2);
+});
+const cubes = numbers.map(function(element) {
+  return Math.pow(element, 3);
+});
+
+const evenNums = numbers.filter(function(element) {
+  return element % 2 === 0;
+});
+const oddNums = numbers.filter(function(element) {
+  return element % 2 !== 0;
+});
+
+const total = numbers.reduce(function(accumulator, element) {
+  return accumulator + element;
+});
+
+console.log(squares);
+console.log(cubes);
+console.log(evenNums);
+console.log(oddNums);
+console.log(total);
+
