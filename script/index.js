8fd24c4c5f94cbf60cc40f5f03622cf09ff66ceb
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-03-13 23:09:25 +0800

    Objects

--- script/index.js
+++ script/index.js
@@ -855,6 +855,42 @@ console.log(oddNumbs);
 console.log(total);
 */
 
+/* 
+object = a collection of related properties and/or methods. Can represent real world objects (people, places) object = {key: value, function()}
 
+const person1 = {
+  firstName: "Spongebob",
+  lastName: "Squarepants",
+  age: 30,
+  isEmployed: true,
+  sayHello: () => console.log("Hello I am Spongebob"),
+  eat: () => console.log("I am eating a Krabby Patty")
+}
+
+const person2 = {
+  firstName: "Patrick",
+  lastName: "Star",
+  age: 42,
+  isEmployed: false,
+  sayHello: () => console.log("Hey, I'm Patrick..."),
+  eat: () => console.log("I am eating a roast beef, chicken, and pizza")
+}
+
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.age);
+console.log(person1.isEmployed); 
+
+console.log(person2.firstName);
+console.log(person2.lastName);
+console.log(person2.age);
+console.log(person2.isEmployed);
+
+
+person1.sayHello();
+person1.eat();
+person2.sayHello();
+person2.eat();
+*/
 
 
