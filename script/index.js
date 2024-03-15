0710882551832a6cb98003166cdbd71d10525bb3
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-03-15 19:18:44 +0800

    Classes

--- script/index.js
+++ script/index.js
@@ -957,3 +957,35 @@ car2.drive();
 car3.drive();
 */
 
+/* 
+class = (ES6 feature) provides a more structured and clearer way to work with objects compared to traditional constructor functions ex. static keyword, encapsulation, inheritance
+
+class Products {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  displayProduct() {
+    console.log(`Product ${this.name}`);
+    console.log(`Price $${this.price.toFixed(2)}`);
+  }
+
+  calculateTotal(salesTax) {
+    return this.price + (this.price * salesTax);
+  }
+}
+
+const salesTax = 0.05;
+
+
+const product1 = new Products("Shirt", 19.99);
+const product2 = new Products("Pants", 22.50);
+const product3 = new Products("Underwear", 100.00);
+
+product3.displayProduct();
+
+const total = product3.calculateTotal(salesTax);
+
+console.log(`The total price (with  tax): $${total.toFixed(2)}`);
+*/
