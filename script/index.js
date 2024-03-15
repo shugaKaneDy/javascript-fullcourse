f2176f28747d00b6355dee05a00b46d97a20952f
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-03-15 21:48:21 +0800

    Inheritance

--- script/index.js
+++ script/index.js
@@ -1046,3 +1046,58 @@ User.getUserCount();
 
 */
 
+/* 
+inheritance = allows a new class to inherit properties and methods from existing classes (parent -> child) helps the code reusability
+
+*/
+
+class Animal {
+  alive = true;
+  eat() {
+    console.log(`This ${this.name} is eating`)
+  }
+  sleep() {
+    console.log(`This ${this.name} is sleeping`)
+  }
+}
+
+class Rabbit extends Animal {
+  name = "Rabit";
+  run() {
+    console.log(`This ${this.name} is running`);
+  }
+}
+class Fish extends Animal {
+  name = "Fish"
+  swim() {
+    console.log(`This ${this.name} is swimming`);
+  }
+}
+class Hawk extends Animal {
+  name = "Hawk";
+  fly() {
+    console.log(`This ${this.name} is flying`);
+  }
+}
+
+
+const rabbit = new Rabbit();
+const fish = new Fish();
+const hawk = new Hawk();
+
+/* rabbit.alive = false; */
+
+console.log(rabbit.alive);
+rabbit.eat();
+rabbit.sleep();
+rabbit.run();
+
+console.log(fish.alive);
+fish.eat();
+fish.sleep();
+fish.swim();
+
+console.log(hawk.alive);
+hawk.eat();
+hawk.sleep();
+hawk.fly();
