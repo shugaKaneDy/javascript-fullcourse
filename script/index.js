ad47971fa21421f7103e90e82c6c86cab60a23e1
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-03-16 20:56:04 +0800

    getter and setter

--- script/index.js
+++ script/index.js
@@ -1162,3 +1162,120 @@ rabbit.run();
 fish.swim();
 hawk.fly();
 */
+
+/* 
+getter = special method that takes a property readable
+setter = special method that takes a property writable
+
+assigning it as setter makes it private, meaning you cannot read it(undefined) when you try to call it on console
+
+assigning a getter will make it read by the console
+
+when using getter it acts like a property meaning you don't need to have () inorder to access it
+
+validate and modify a value when reading/writing a property
+
+
+
+class Rectangle {
+
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  set width(newWidth) {
+    if (newWidth > 0) {
+      this._width = newWidth;
+    }
+    else {
+      console.error(`Width must be positive number`);
+    }
+  }
+
+  set height(newHeight) {
+    if (newHeight > 0) {
+      this._height = newHeight;
+    }
+    else {
+      console.error(`Height must be positive number`);
+    }
+  }
+
+  get width() {
+    return `${this._width.toFixed(1)}cm`;
+  }
+
+  get height() {
+    return `${this._height.toFixed(1)}cm`;
+  }
+
+  get area() {
+    return `${(this._width * this._height).toFixed(1)}cm^2`;
+  }
+
+}
+
+const rectangle = new Rectangle(3, 4);
+
+console.log(rectangle.width);
+console.log(rectangle.height);
+console.log(rectangle.area);
+
+class Person {
+
+  constructor(firstname, lastname, age) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.age = age;
+  }
+
+  set firstname(newFirstname) {
+    if(typeof newFirstname === 'string' && newFirstname.length > 0) {
+      this._firstname = newFirstname;
+    } else {
+      console.error("first name must be a non-empty string");
+    }
+  }
+
+  set lastname(newLastname) {
+    if(typeof newLastname === 'string' && newLastname.length > 0) {
+      this._lastname = newLastname;
+    } else {
+      console.error("last name must be a non-empty string");
+    }
+  }
+
+  set age(newAge) {
+    if (typeof newAge === "number" && newAge >= 0) {
+      this._age = newAge;
+    } else {
+      console.error("Age must non negative number");
+    }
+  }
+
+  get firstname() {
+    return this._firstname;
+  }
+
+  get lastname() {
+    return this._lastname;
+  }
+
+  get fullname() {
+    return this._firstname + " " + this._lastname;
+  }
+
+  get age() {
+    return this._age;
+  }
+}
+
+const person = new Person("Spongebob", "Squarepants", 30);
+
+console.log(person.firstname);
+console.log(person.lastname);
+console.log(person.fullname);
+console.log(person.age);
+*/
+
