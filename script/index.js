c4423fe8fe97818e1b0050e037379e181161081c
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-03-16 10:39:39 +0800

    Super

--- script/index.js
+++ script/index.js
@@ -1100,3 +1100,65 @@ hawk.eat();
 hawk.sleep();
 hawk.fly();
 */
+
+/* 
+super = keyword is used in classes to call the constructor or access the properties and methods of a parent (super class) this = this object, super = the parent
+
+instead of creating a value onto the class itself, we will pass it onto constructor. 
+
+class Animal {
+
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  move(speed) {
+    console.log(`The ${this.name} moves at a speed of ${speed}mph `)
+  }
+}
+class Rabbit extends Animal {
+
+  constructor(name, age, runSpeed) {
+    super(name, age);
+    this.runSpeed = runSpeed;
+  }
+
+  run() {
+    console.log(`This ${this.name} can run`)
+    super.move(this.runSpeed);
+  }
+}
+class Fish extends Animal {
+
+  constructor(name, age, swimSpeed) {
+    super(name, age);
+    this.swimSpeed = swimSpeed;
+  }
+
+  swim() {
+    console.log(`This ${this.name} can swim`)
+    super.move(this.swimSpeed);
+  }
+}
+class Hawk extends Animal {
+
+  constructor(name, age, flySpeed) {
+    super(name, age);
+    this.flySpeed = flySpeed; 
+  }
+
+  fly() {
+    console.log(`This ${this.name} can fly`);
+    super.move(this.flySpeed);
+  }
+}
+
+const rabbit = new Rabbit("rabbit", 1, 25);
+const fish = new Fish("fish", 2, 12);
+const hawk = new Hawk("hawk", 3, 50);
+
+rabbit.run();
+fish.swim();
+hawk.fly();
+*/
