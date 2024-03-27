403421648ae3f8a2468c40093885925e5ea8c79d
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-03-27 21:55:40 +0800

    BT 5 Navbars

--- script/index.js
+++ script/index.js
@@ -1501,3 +1501,25 @@ console.log(numbers); */
 people.sort((a, b) => a.name.localeCompare(b.name));
 
 console.log(people) */
+
+/* 
+Shaffle Array
+
+Fisher-Yates Algorithm
+
+const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+
+shaffle(cards);
+
+console.log(cards);
+
+function shaffle(array) {
+  for(let i = array.length - 1; i > 0; i--) {
+    const random = Math.floor(Math.random() * (i + 1));
+
+    [array[i], array[random]] = [array[random], array[i]];
+  }
+}
+*/
+
+
