3c2d30026362e667fba9f91f21392916186231f3
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-03-28 10:22:41 +0800

    Date objects

--- script/index.js
+++ script/index.js
@@ -1522,4 +1522,47 @@ function shaffle(array) {
 }
 */
 
+/* 
+Date of Objects = Objects that contain values that represent dates and times These date of objects can be changed and formatted
 
+// Date(year, month, day, hour, minute, second, ms)
+
+/* const date = new Date(2024, 0, 1, 2, 3, 4, 5); */
+/* const date = new Date("2024-01-02T12:00:00Z"); */
+
+
+/* const date = new Date(); */
+
+
+/* const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const hour = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const dayOfWeek = date.getDay();
+
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hour);
+console.log(minutes);
+console.log(seconds);
+console.log(dayOfWeek); */
+
+/* date.setFullYear(2026)
+date.setMonth(0);
+date.setDate(1);
+date.setHours(2);
+date.setMinutes(3);
+date.setSeconds(4);
+
+console.log(date); */
+
+/* const date1 = new Date("2023-12-31");
+const date2 = new Date("2024-01-01");
+
+if (date2 > date1) {
+  console.log("Happy new year!");
+} 
+*/
