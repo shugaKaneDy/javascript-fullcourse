af8698c5aa52a683f6bd1dea01e18a699a74446f
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-04-07 21:41:58 +0800

    ES6 Module

--- script/index.js
+++ script/index.js
@@ -1872,3 +1872,19 @@ resetBtn.addEventListener("click", reset);
 */
 
 
+/* 
+
+ES6 Module = An external file that contains reusable code that can be imported into other JavaScript files. Write reusable codes for many different apps. Can contain variables, classes, functions ... and more Introduced as part of ECMAScript 2015 update.
+
+*/
+
+import {PI, gerCircumference, getArea, getVolume} from './mathUtil.js';
+
+console.log(PI);
+const circumference = gerCircumference(10);
+const area = getArea(10);
+const volume = getVolume(10);
+
+console.log(`${circumference.toFixed(2)}cm`);
+console.log(`${area.toFixed(2)}cm^2`);
+console.log(`${volume.toFixed(2)}cm^3`);
