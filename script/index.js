96d9e5a68d6c70545fe39f346c659b48bfee1b1c
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-04-15 21:51:50 +0800

    Element Selectors

--- script/index.js
+++ script/index.js
@@ -2080,3 +2080,68 @@ const welcomeMessage = document.getElementById("welcome-msg");
 
 welcomeMessage.textContent += username === "" ? 'Guest' : username;
 */
+
+/* 
+
+element selectos = Methods used to target and manipulate HTML elements They allow you to select one or multiple HTML elements from DOM (Document Object Model)
+
+1. document.getElementById()      // ELEMENT OR NULL
+2. document.getElementClassName() // HTML COLLECTION // live
+3. document.getElementByTagName() // HTML COLLECTION // live
+4. document.querySelector()       // ELEMENT OR NULL
+5. document.querySelectorAll()    // NODELIST // static 
+
+const myHeading = document.getElementById("my-heading");
+myHeading.style.backgroundColor = "yellow";
+myHeading.style.textAlign = "center";
+
+console.log(myHeading);
+
+const fruits = document.getElementsByClassName("fruits");
+
+fruits[0].style.backgroundColor = "yellow";
+fruits[1].style.backgroundColor = "orange"; 
+for (let fruit of fruits) {
+  fruit.style.backgroundColor = "yellow";
+}
+Array.from(fruits).forEach(fruit => {
+  fruit.style.backgroundColor = "yellow";
+});
+
+const h4Elements = document.getElementsByTagName("h4");
+const liElements = document.getElementsByTagName("li");
+
+for (let h4Element of h4Elements) {
+  h4Element.style.backgroundColor = "yellow";
+}
+Array.from(h4Elements).forEach(h4Element => {
+  h4Element.style.backgroundColor = "yellow";
+});
+
+for (let liElement of liElements) {
+  liElement.style.backgroundColor = "lightgreen";
+} 
+Array.from(liElements).forEach(liElement => {
+  liElement.style.backgroundColor = "lightgreen";
+});
+
+const element = document.querySelector(".fruits");
+
+element.style.backgroundColor = "yellow";
+
+const fruits = document.querySelectorAll(".fruits"); 
+fruits[0].style.backgroundColor = "yellow";
+const foods = document.querySelectorAll("li");
+
+foods.forEach(food => {
+  food.style.backgroundColor = "yellow";
+});
+
+console.log(foods);
+*/
+
+
+
+
+
+
