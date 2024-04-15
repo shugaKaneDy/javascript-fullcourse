14143511fda22f674d171c407c037c7ed66e394d
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-04-15 21:25:10 +0800

    DOM

--- script/index.js
+++ script/index.js
@@ -2063,3 +2063,20 @@ function calculate() {
   }
 }
 */
+
+/* 
+
+DOM = DOCUMENT OBJECT MODEL
+
+- object {} that represents the page you see in the web browser and provides you with an API interaction with it. Web browser constructs the DOM when it loads an HTML document, and structures all the elements in a tree-like representation. JavaScript can access the DOM to dynamically change the content, structure, and style of a web page.
+
+document.title = "My website";
+document.body.style.backgroundColor = "hsl(0, 0%, 15%)";
+
+console.dir(document);
+
+const username = "Kane Pogi";
+const welcomeMessage = document.getElementById("welcome-msg");
+
+welcomeMessage.textContent += username === "" ? 'Guest' : username;
+*/
