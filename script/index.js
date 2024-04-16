ff8572ced386e4639742daaf4372bc7dd5e68f07
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-04-16 21:16:18 +0800

    DOM Navigation

--- script/index.js
+++ script/index.js
@@ -2140,8 +2140,79 @@ foods.forEach(food => {
 console.log(foods);
 */
 
+/* 
+
+DOM Navigation = The process of navigating through the structure of the HTML document using JavaScript
+
+.firstElementChild
+.lastElementChild
+.nextElementSibling
+.previousElementSibling
+.parentElement
+.children
+
+*/
 
 
+/* 
+// Fist Child
+
+const element = document.getElementById("vegetables");
+
+const firstChild = element.firstElementChild;
+firstChild.style.backgroundColor = "Yellow"; 
+
+const ulElements = document.querySelectorAll("ul");
+
+ulElements.forEach(ulElement => {
+  const firstChild = ulElement.firstElementChild;
+  firstChild.style.backgroundColor = "Yellow";
+});
+
+// Last Element
+
+const element = document.getElementById("desserts");
+const lastChild = element.lastElementChild;
+
+lastChild.style.backgroundColor = "Yellow";
+
+const ulElements = document.querySelectorAll("ul");
+
+ulElements.forEach(ulElement => {
+  const lastChild = ulElement.lastElementChild;
+  lastChild.style.backgroundColor = "Yellow";
+
+});
 
 
+// Next Sibling
 
+const element = document.getElementById("apple");
+const nextSibling = element.nextElementSibling;
+
+nextSibling.style.backgroundColor = "yellow"
+
+// Previous Sibling
+
+const element = document.getElementById("orange");
+const prevSibling = element.previousElementSibling;
+prevSibling.style.backgroundColor = "Yellow"
+
+// Parent
+
+const element = document.getElementById("apple");
+const parent = element.parentElement;
+
+parent.style.backgroundColor = "Yellow"
+
+// Children
+
+const element = document.getElementById("fruits");
+const children = element.children;
+
+children[1].style.backgroundColor = "Yellow";
+
+Array.from(children).forEach(child => {
+  child.style.backgroundColor = "Yellow"
+})
+*/
