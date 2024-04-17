44df0d770ac5953e9db05ef2647959540758a4da
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-04-17 19:09:29 +0800

    Add and Change HTML Elements

--- script/index.js
+++ script/index.js
@@ -2216,3 +2216,54 @@ Array.from(children).forEach(child => {
   child.style.backgroundColor = "Yellow"
 })
 */
+
+/* 
+
+Add and Change HTML Elements in Javascript 
+
+// EXAMPLE 1 <h1>
+
+// STEP 1 CREATE THE ELEMENT
+//const newH1 = document.createElement("h1");
+
+const newListItem = document.createElement("li");
+
+// STEP 2 ADD ATTRIBUTES/PROPERTIES
+//newH1.textContent = "I like Pizza"
+//newH1.id = "myH1";
+//newH1.style.color = "tomato";
+//newH1.style.textAlign = "center";
+newListItem.textContent = "coconut";
+newListItem.id = "coconut";
+newListItem.classList.add("list-group-item");
+newListItem.style.fontWeight = "bold";
+newListItem.style.backgroundColor = "lightgreen";
+
+// STEP 3 APPEND ELEMENT TO THE DOM
+//document.body.append(newH1);
+//document.body.prepend(newH1);
+//document.getElementById("box-1").append(newH1);
+//document.getElementById("box-2").prepend(newH1);
+
+//const box2 = document.getElementById("box-2");
+//document.body.insertBefore(newH1, box2);
+
+//const boxes = document.querySelectorAll(".box");
+//document.body.insertBefore(newH1, boxes[2]);
+
+document.getElementById("fruits").append(newListItem);
+//document.getElementById("fruits").prepend(newListItem);
+
+//const orange = document.getElementById("orange");
+//document.getElementById("fruits").insertBefore(newListItem, orange);
+
+//const listItems = document.querySelectorAll("#fruits li");
+//document.getElementById("fruits").insertBefore(newListItem, listItems[1]);
+
+
+// REMOVE HTML ELEMENT
+//document.getElementById("box-1").removeChild(newH1);
+document.getElementById("fruits").removeChild(newListItem);
+
+*/
+
