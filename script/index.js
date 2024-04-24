ca3156e963024674c53cddd11e15d6a048b279f6
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-04-24 21:34:50 +0800

    Rock Paper Scissors

--- script/index.js
+++ script/index.js
@@ -2543,3 +2543,73 @@ buttons.forEach(button => {
   });
 })
 */
+
+/* 
+Rock Paper Scissors
+
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
+let playerScore = 0;
+let computerScore = 0;
+
+rock.addEventListener("click", () => {
+  playGame("rock");
+});
+paper.addEventListener("click", () => {
+  playGame("paper");
+});
+scissors.addEventListener("click", () => {
+  playGame("scissors");
+});
+
+function playGame(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
+
+  if (playerChoice === computerChoice) {
+    result = "It's a tie!";
+  } else {
+    switch (playerChoice) {
+      case "rock":
+        result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE";
+        break
+      case "paper":
+        result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE";
+        break
+      case "scissors":
+        result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE";
+        break
+    }
+  }
+
+  playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+  computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+  resultDisplay.textContent = result;
+
+  resultDisplay.classList.remove("text-success", "text-danger");
+
+  switch (result) {
+    case "YOU WIN!":
+      resultDisplay.classList.add("text-success");
+      playerScore++;
+      playerScoreDisplay.textContent = playerScore;
+      break;
+    case "YOU LOSE":
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
+      resultDisplay.classList.add("text-danger");
+      break;
+  }
+}
+
+*/
