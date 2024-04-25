b59954ca0d91f91edbc7b1cc30223b5dfe36e6a2
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-04-25 22:23:04 +0800

    Image Slider

--- script/index.js
+++ script/index.js
@@ -2613,3 +2613,52 @@ function playGame(playerChoice) {
 }
 
 */
+
+/* 
+
+Image Slider
+
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+prev.addEventListener("click", () => {
+  prevSlide();
+});
+next.addEventListener("click", () => {
+  nextSlide();
+});
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+
+  if(slides.length > 0) {
+    slides[slideIndex].classList.add("displaySlide");
+    intervalId =  setInterval(nextSlide, 5000);
+  }
+}
+function showSlide(index) {
+  if(index >= slides.length) {
+    slideIndex = 0;
+  } else if(index < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slides.forEach(slide => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slideIndex].classList.add("displaySlide");
+}
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
+}
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
+*/
