dde5075eeb52875c2b86ddc5af29548cd06acd5e
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-04-26 09:11:20 +0800

    Callback Hell

--- script/index.js
+++ script/index.js
@@ -2662,3 +2662,45 @@ function nextSlide() {
   showSlide(slideIndex);
 }
 */
+
+/* 
+Callback Hell = Situation in JavaScript where callbacks are pested within other callbacks to the degree where code is difficult to read. Old pattern to handle assychronous functions. Use Promises + async/await to avoid Callback Hell.
+
+function task1(callback) {
+
+  setTimeout(() => {
+    console.log("Task 1 Complete");
+    callback();
+  }, 2000);
+}
+function task2(callback) {
+  setTimeout(() => {
+    console.log("Task 2 Complete");
+    callback();
+  }, 1000);
+}
+function task3(callback) {
+  setTimeout(() => {
+    console.log("Task 3 Complete");
+    callback();
+  }, 3000);
+}
+function task4(callback) {
+  setTimeout(() => {
+    console.log("Task 4 Complete");
+    callback();
+  }, 1500);
+}
+
+task1(() => {
+  task2(() => {
+    task3(() => {
+      task4(() => {
+        console.log("All Task Completed");
+      });
+    });
+  });
+});
+*/
+
+
