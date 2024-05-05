c6b85b777292ef26c9019c2fbd722d31c6ff1002
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-05-05 12:53:05 +0800

    Async/Await

--- script/index.js
+++ script/index.js
@@ -2795,3 +2795,81 @@ walkDog().then(value => {console.log(value); return cleanKitchen()})
           .then(value => {console.log(value); console.log("You finish all the chores")})
           .catch(error => console.error(error));
 */
+
+
+/* 
+Async/Await = Async = makes function return a promise
+              Await = makes an async function wait for a promise
+
+- Allows you write asynchronous code in a synchronous manner. Async doesn't have resolve or reject parameters. Everything after Await is placed in an event queue
+
+function walkDog () {
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+
+      const dogwalked = true;
+      
+      if (dogwalked) {
+        resolve("You walk the dog");
+      } else {
+        reject("You didn't walk the dog");
+      }
+      
+    }, 1500);
+  });
+}
+function cleanKitchen () {
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+
+      const kitchenCleaned = true;
+      if(kitchenCleaned) {
+        resolve("You clean the kitchen bro");
+      } else {
+        reject("You didn't clean the kitchen");
+      }
+    }, 2500);
+  });
+}
+
+function takeOutTrash () {
+
+  return new Promise((resolve, reject) => {
+
+    const trashTakenOut = true;
+
+    if(trashTakenOut) {
+      resolve("You take out the trash");
+    } else {
+      reject("You didn't take out the trash");      
+    }
+    setTimeout(() => {
+    }, 500);
+  });
+}
+
+async function doChores () {
+
+  try {
+    const walkDogResult = await walkDog();
+    console.log(walkDogResult);
+  
+    const cleanKitchenResult = await cleanKitchen();
+    console.log(cleanKitchenResult);
+  
+    const takeOutTrashResult = await takeOutTrash();
+    console.log(takeOutTrashResult);
+  
+    console.log("You finished all the chores");
+
+  } catch (error) {
+    console.error(error);
+  }
+
+
+}
+
+doChores();
+*/
